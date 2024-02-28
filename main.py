eb98f089556769ba142f8c8b6294e1d66647525f
Kleber <kleber@gmail.com>
2024-02-28 11:46:27 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+num1 = float (input ("digite o primeiro cateto:"))
+num2 = float (input ("digite o segundo cateto:"))
+resultado = (num2 - num1)
+hipotenusa = (resultado + num2)
+
+print (f"a hipotenusa é igual a:{hipotenusa}")
